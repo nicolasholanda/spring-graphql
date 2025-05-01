6e0314f38faea83abcec7b33eba97051b749a65d
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-05-01 10:45:47 -0300

    Initializing database

--- src/main/java/com/github/nicolasholanda/spring_graphql/repository/AuthorRepository.java
+++ src/main/java/com/github/nicolasholanda/spring_graphql/repository/AuthorRepository.java
@@ -2,5 +2,7 @@ package com.github.nicolasholanda.spring_graphql.repository;
 
 import com.github.nicolasholanda.spring_graphql.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {}
--- src/main/java/com/github/nicolasholanda/spring_graphql/repository/BookRepository.java
+++ src/main/java/com/github/nicolasholanda/spring_graphql/repository/BookRepository.java
@@ -2,5 +2,7 @@ package com.github.nicolasholanda.spring_graphql.repository;
 
 import com.github.nicolasholanda.spring_graphql.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {}
